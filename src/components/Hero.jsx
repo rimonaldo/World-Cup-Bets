de0afb7e2596ec5a07edd03b7e7927d6840373e3
Rimon <rimons2804@gmail.com>
2022-10-23 00:39:37 +0300

    type template - display-body , hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import WcLogo from './WcLogo'
+import TemplateDisplayBody from './Templates/Template_DisplayBody'
 export default function Hero() {
    return (
       <section className="hero-container">
-         <div className="text-style">
-            <h1 className="display-exp">World Cup Bets</h1>
-            <p className="body">
-               World Cup Bets takes all your money in promise that you will win more money from it. you are probably
-               going to lose all of it because you are a mug.
-            </p>
-         </div>
+         <TemplateDisplayBody />
          <WcLogo />
       </section>
    )
